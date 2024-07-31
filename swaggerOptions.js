c06a01be6feead1971a0f5950f43bf62d081528e
Marcelo Moraes <marcelo.moraes@medcof.tech>
2024-07-31 01:31:43 -0300

    teste swagger 3

--- swaggerOptions.js
+++ swaggerOptions.js
@@ -1,38 +1,23 @@
-// // swaggerOptions.js
-// const swaggerJsdoc = require('swagger-jsdoc');
-
-// const options = {
-//   definition: {
-//     openapi: '3.0.0',
-//     info: {
-//       title: 'API de Personagens',
-//       version: '1.0.0',
-//       description: 'Documentação da API de personagens com Swagger',
-//     },
-//     servers: [
-//       {
-//         url: 'https://one-piece-backend-nodejs.vercel.app/', // Substitua pelo URL do seu projeto Vercel
-//         description: 'Servidor principal',
-//       },
-//     ],
-//   },
-//   apis: ['./index.js'], // Ou o caminho para os arquivos onde você tem as anotações Swagger
-// };
-
-// const swaggerSpec = swaggerJsdoc(options);
-// module.exports = swaggerSpec;
+// swaggerOptions.js
 const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 
-const swaggerSpec = swaggerJsdoc({
+const options = {
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'API de Personagens',
       version: '1.0.0',
+      description: 'Documentação da API de personagens com Swagger',
     },
+    servers: [
+      {
+        url: 'https://one-piece-backend-nodejs.vercel.app/', // Substitua pelo URL do seu projeto Vercel
+        description: 'Servidor principal',
+      },
+    ],
   },
-  apis: ['./index.js'], // Caminho para os arquivos de anotação
-});
+  apis: ['./index.js'], // Ou o caminho para os arquivos onde você tem as anotações Swagger
+};
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const swaggerSpec = swaggerJsdoc(options);
+module.exports = swaggerSpec;
