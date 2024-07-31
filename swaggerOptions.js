d750acd94415407a0aa955bc55df936700e89fbe
Marcelo Moraes <marcelo.moraes@medcof.tech>
2024-07-31 00:43:07 -0300

    Adicionar Swagger UI

--- swaggerOptions.js
+++ swaggerOptions.js
@@ -1,17 +1,23 @@
+// swaggerOptions.js
 const swaggerJsdoc = require('swagger-jsdoc');
 
-const swaggerOptions = {
+const options = {
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'API de Personagens',
       version: '1.0.0',
-      description: 'Documentação da API de Personagens do One Piece',
+      description: 'Documentação da API de personagens com Swagger',
     },
+    servers: [
+      {
+        url: 'https://one-piece-backend-nodejs.vercel.app/', // Substitua pelo URL do seu projeto Vercel
+        description: 'Servidor principal',
+      },
+    ],
   },
-  apis: ['./index.js'], // Caminho para os arquivos com anotações Swagger
+  apis: ['./index.js'], // Ou o caminho para os arquivos onde você tem as anotações Swagger
 };
 
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
+const swaggerSpec = swaggerJsdoc(options);
 module.exports = swaggerSpec;
